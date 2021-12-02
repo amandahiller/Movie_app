2156b548c69a9f9fb36bcf73bb907f0974d7caa4
Amanda Speroni-Hiller <amandasperoni@icloud.com>
2021-12-02 14:18:40 -0800

    Added id for avengers movie

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fetchData = async () => {
     const response = await axios.get('http://www.omdbapi.com/', {
         params: {
             apikey: 'f7a3b638',
-            s: 'avengers'
+            i: 'tt0848228'
         }
     });
 
