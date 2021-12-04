d8858a5b1d047902dfa691f42df7e3a799d7e93b
Amanda Speroni-Hiller <amandasperoni@icloud.com>
2021-12-04 15:54:27 -0800

    Fixed broken images

--- index.js
+++ index.js
@@ -31,14 +31,15 @@ const resultsWrapper = document.querySelector('.results');
 const onInput = async event => {  
     const movies = await fetchData(event.target.value);
     
-    resultsWrapper.innerHTML = ' ';
+    resultsWrapper.innerHTML = '';
     dropdown.classList.add('is-active');
     for (let movie of movies) {
         const option = document.createElement('a');
+        const imgSrc = item.Poster === 'N/A' ? '' : item.Poster;
 
         option.classList.add('dropdown-item');
         option.innerHTML = `
-         <img src=${movie.Poster} />
+         <img src=${imgSrc} />
          ${movie.Title}
         `;
 
